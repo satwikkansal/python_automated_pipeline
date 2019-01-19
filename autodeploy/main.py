8f2cb1f9962bd911da16afbaca8aeb6abf2de429
Satwik Kansal <satwik.kansal@myntra.com>
2019-01-19 12:02:02 +0530

    Use division instead of products, and refactor into a package

---
+++ autodeploy/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/division')
+def get_division():
+    x = request.args.get('x')
+    y = request.args.get('y')
+    return compute_division(x, y)
+
+
+def compute_division(x, y):
+    if y == 0:
+        return float('inf')
+    return x / y
+
+
+if __name__ == "__main__":
+    app.run()
