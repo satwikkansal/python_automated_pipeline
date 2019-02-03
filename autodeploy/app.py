67dd5e5d5a0b940db9795dde5e39149a2e3cdc6c
Satwik Kansal <>
2019-02-03 19:39:07 +0530

    Fix failing tests and rename module

--- autodeploy/app.py
+++ autodeploy/app.py
@@ -12,16 +12,12 @@ def hello_world():
 
 @app.route('/division')
 def get_division():
-    x = request.args.get('x')
-    y = request.args.get('y')
-    return compute_division(x, y)
+    x = float(request.args.get('x'))
+    y = float(request.args.get('y'))
+    return str(compute_division(x, y))
 
 
 def compute_division(x, y):
     if y == 0:
         return float('inf')
     return x / y
-
-
-if __name__ == "__main__":
-    app.run()
