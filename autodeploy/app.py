27e438531bbd54f4bafe9b5870e72774cb897784
Satwik Kansal <>
2019-02-11 01:52:57 +0530

    Test autopep8

--- autodeploy/app.py
+++ autodeploy/app.py
@@ -1,23 +1,19 @@
 from __future__ import division
-
 from flask import Flask, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
 @app.route('/division')
 def get_division():
     x = float(request.args.get('x'))
     y = float(request.args.get('y'))
-    return str(compute_division(x, y))
-
+    return str(compute_division(x,y))
 
 def compute_division(x, y):
     if y == 0:
         return float('inf')
-    return x / y
+    return x/y
