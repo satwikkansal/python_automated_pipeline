c9bc141ec077cc0f111ae39e64cee7f4be603836
Satwik Kansal <>
2019-02-12 01:15:16 +0530

    Autopep8 works :tada:

--- autodeploy/app.py
+++ autodeploy/app.py
@@ -3,18 +3,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return "Hello World!"
+
 
 @app.route('/division')
 def get_division():
     x = float(request.args.get('x'))
     y = float(request.args.get('y'))
-    return str(compute_division(x,y))
+    return str(compute_division(x, y))
+
 
 def compute_division(x, y):
-	#return infinity in case of divide by zero
+        # return infinity in case of divide by zero
     if y == 0:
         return float('inf')
     return x / y
